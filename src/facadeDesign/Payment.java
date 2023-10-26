b23a25f909ad3195eb0eb64d7ffd732ba8bc27a1
carpe <carpe@DESKTOP-OVAK946.attlocal.net>
2023-10-25 21:46:36 -0500

    Basics for the payment class, still needs to be finished after order class for processing the payment

--- src/facadeDesign/Payment.java
+++ src/facadeDesign/Payment.java
@@ -6,6 +6,49 @@ package facadeDesign;
 
 public class Payment {
 	
-	private double amt;
+	private double amount;
+	private String drivingRecipient;
+	private String paymentType;
+	private boolean processed;
 	
+	public Payment(double amount, String drivingRecipient, String paymentType) {
+		this.amount = amount;
+		this.drivingRecipient = drivingRecipient;
+		this.paymentType = paymentType;
+		this.processed = false;
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+	
+	public String getDrvingRecipient() {
+		return drivingRecipient;
+	}
+	
+	public String getPaymentType() {
+		return paymentType;
+	}
+	
+	public void processPayment() {
+		if(!processed) {
+			//come back to this logic after Order class
+			processed = true;
+		} else {
+			System.out.println("Payment has already been sent!");
+		}
+		
+	}
+	
+	public boolean isProcessed() {
+		return processed;
+	}
+	
+	public String toString() {
+		return "Payment\n" + 
+				"Amount = " + amount + 
+				", Payment Type = " + paymentType +
+				", Driving Recipient = " + drivingRecipient +
+				", Is the payment processed? " + processed;
+	}
 }
