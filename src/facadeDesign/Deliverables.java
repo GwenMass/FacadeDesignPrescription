9b79efcadf27c9048fc04f7405ed67f7aef85a39
carpe <carpe@DESKTOP-OVAK946.attlocal.net>
2023-10-25 22:32:59 -0500

    itemDescription changed to itemName

--- src/facadeDesign/Deliverables.java
+++ src/facadeDesign/Deliverables.java
@@ -1,32 +1,32 @@
 package facadeDesign;
 
 public enum Deliverables {
-	BURGER("Order Burger", 9.99),							//Item 1
-	PIZZA("Order Pizza", 7.99),								//Item 2
-	TACOS("Order 3 Tacos", 3.99),							//Item 3
-	SALAD("Order Salad", 5.95),								//Item 4
-	RAMEN("Order Ramen", 3.50),								//Item 5
-	CHICKENFINGERS("Order Chicken Fingers",8.99),			//Item 6
-	CHICKENSANDWICH("Order Chicken Sandwich", 8.95),		//Item 7
-	SUSHI("Order Sushi", 9.65),								//Item 8
-	SODA("Order Soda", 2.99),								//Item 9
-	TEA("Order Tea", 1.99),									//Item 10
-	COFFEE("Order Coffee", 5.65),							//Item 11
-	MILKSHAKE("Order Milkshake", 4.95),						//Item 12
-	DONUTS("Order Dozen Donuts", 9.45),						//Item 13
-	FRIES("Order Fries", 3.99),								//Item 14
-	HOTDOG("Order Hotdog", 6.95);							//Item 15
+	BURGER("Burger", 9.99),							//Item 1
+	PIZZA("Pizza", 7.99),							//Item 2
+	TACOS("Tacos", 3.99),							//Item 3
+	SALAD("Salad", 5.95),							//Item 4
+	RAMEN("Ramen", 3.50),							//Item 5
+	CHICKENFINGERS("Chicken Fingers",8.99),			//Item 6
+	CHICKENSANDWICH("Chicken Sandwich", 8.95),		//Item 7
+	SUSHI("Sushi", 9.65),							//Item 8
+	SODA("Soda", 2.99),								//Item 9
+	TEA("Tea", 1.99),								//Item 10
+	COFFEE("Coffee", 5.65),							//Item 11
+	MILKSHAKE("Milkshake", 4.95),					//Item 12
+	DONUTS("Dozen Donuts", 9.45),					//Item 13
+	FRIES("Fries", 3.99),							//Item 14
+	HOTDOG("Hotdog", 6.95);							//Item 15
 	
-	private String itemDescription;
+	private String itemName;
 	private double price;
 	
-	Deliverables(String itemDescription, double price){
-		this.itemDescription = itemDescription;
+	Deliverables(String itemName, double price){
+		this.itemName = itemName;
 		this.price = price;
 	}
 	
 	public String getItemDescription() {
-		return itemDescription;
+		return itemName;
 	}
 	
 	public double getPrice() {
